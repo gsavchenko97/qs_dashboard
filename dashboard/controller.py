370ccedd76e85bb575c0df65c8635d3f33e0c4d0
BorisOne <boris95714@gmail.com>
2020-05-26 01:02:59 +0300

    added application windows manager

---
+++ dashboard/controller.py
@@ -0,0 +1,18 @@
+from dashboard.dialog import DialogWindow
+from dashboard.gui import MainWindow
+
+
+class Controller:
+    """
+    Allows to manage multiple windows of the application
+    """
+    def show_dialog_window(self):
+        self.dialog_window = DialogWindow()
+        self.dialog_window.switch_window.connect(self.show_main_window)
+        self.dialog_window.show()
+
+    def show_main_window(self):
+        self.main_window = MainWindow()
+        self.main_window.switch_window.connect(self.show_dialog_window)
+        self.dialog_window.close()
+        self.main_window.show()
