7d7e28fc88f80362f943847085f4c3b7ebaa25e7
BorisOne <boris95714@gmail.com>
2020-05-31 15:16:08 +0300

    the login window opens first

--- main.py
+++ main.py
@@ -5,5 +5,6 @@ from dashboard.controller import Controller
 if __name__ == "__main__":
     app = QApplication([])
     controller = Controller()
-    controller.show_dialog_window()
+    # controller.show_dialog_window()
+    controller.show_login_window()
     app.exec_()
