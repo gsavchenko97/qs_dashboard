17f47ca575d851092bb6ce535031482c48bb5125
BorisOne <boris95714@gmail.com>
2020-06-05 13:56:39 +0300

    added max length for username and password

--- dashboard/login.py
+++ dashboard/login.py
@@ -1,12 +1,16 @@
 from PyQt5.QtWidgets import (QDialog, QPushButton, QLineEdit, QGridLayout, QMessageBox)
 from PyQt5.QtCore import pyqtSignal
 
+from dashboard.utils.user import (
+    check_if_user_exists, MAX_PASSWORD_LENGTH, MAX_USERNAME_LENGTH
+)
+
 
 class Login(QDialog):
     """
     Log In form
     """
-    show_main_window = pyqtSignal(str, object)
+    show_main_window = pyqtSignal(str, str, object)
     show_signup_window = pyqtSignal(object)
 
     def __init__(self, parent=None):
@@ -18,11 +22,13 @@ class Login(QDialog):
 
         self.line_edit_username = QLineEdit()
         self.line_edit_username.setPlaceholderText('Username')
+        self.line_edit_username.setMaxLength(MAX_USERNAME_LENGTH)
         self.line_edit_username.setFixedHeight(35)
         layout.addWidget(self.line_edit_username, 0, 0, 1, 2)
 
         self.line_edit_password = QLineEdit()
         self.line_edit_password.setPlaceholderText('Password')
+        self.line_edit_password.setMaxLength(MAX_PASSWORD_LENGTH)
         self.line_edit_password.setFixedHeight(35)
         layout.addWidget(self.line_edit_password, 1, 0, 1, 2)
 
@@ -37,14 +43,17 @@ class Login(QDialog):
         self.setLayout(layout)
 
     def handle_login(self):
-        msg = QMessageBox()
+        username = self.line_edit_username.text()
+        password = self.line_edit_password.text()
 
         def fake_check():
-            return self.line_edit_username.text() == '' and self.line_edit_password.text() == ''
+            return username == '' and password == ''
 
+        # if check_if_user_exists():
         if fake_check():
-            self.show_main_window.emit("add_data", self)
+            self.show_main_window.emit(username, "add_data", self)
         else:
+            msg = QMessageBox()
             msg.setText("Incorrect Username or Password")
             msg.exec_()
 
