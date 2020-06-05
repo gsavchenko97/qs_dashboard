01650c2fe366ffa2d83782ca55905bcbee9c3c49
BorisOne <boris95714@gmail.com>
2020-06-05 13:57:47 +0300

    added checking of given user information

--- dashboard/signup.py
+++ dashboard/signup.py
@@ -1,5 +1,13 @@
-from PyQt5.QtWidgets import (QDialog, QPushButton, QRadioButton, QLineEdit, QGridLayout)
+from PyQt5.QtWidgets import (
+    QDialog, QPushButton, QRadioButton, QLineEdit, QGridLayout, QMessageBox
+)
 from PyQt5.QtCore import pyqtSignal
+
+from dashboard.utils.user import (
+    create_new_user,
+    check_password, check_username, check_firstname,
+    MAX_PASSWORD_LENGTH, MAX_USERNAME_LENGTH, MAX_FIRSTNAME_LENGTH
+)
 
 
 class Signup(QDialog):
@@ -20,16 +28,19 @@ class Signup(QDialog):
 
         self.line_edit_username = QLineEdit()
         self.line_edit_username.setPlaceholderText('Enter your username')
+        self.line_edit_username.setMaxLength(MAX_USERNAME_LENGTH)
         self.line_edit_username.setFixedHeight(35)
         layout.addWidget(self.line_edit_username, 0, 0, 1, 3)
 
         self.line_edit_firstname = QLineEdit()
         self.line_edit_firstname.setPlaceholderText('Enter your first name')
+        self.line_edit_firstname.setMaxLength(MAX_FIRSTNAME_LENGTH)
         self.line_edit_firstname.setFixedHeight(35)
         layout.addWidget(self.line_edit_firstname, 1, 0, 1, 3)
 
         self.line_edit_password = QLineEdit()
         self.line_edit_password.setPlaceholderText('Enter your new password')
+        self.line_edit_password.setMaxLength(MAX_PASSWORD_LENGTH)
         self.line_edit_password.setFixedHeight(35)
         layout.addWidget(self.line_edit_password, 2, 0, 1, 3)
 
@@ -57,13 +68,48 @@ class Signup(QDialog):
         self.show_login_window.emit(self)
 
     def handle_signup(self):
+        username = self.line_edit_username.text()
+        password = self.line_edit_password.text()
+        firstname = self.line_edit_firstname.text()
+        gender = self.gender
+
+        messages = []
+        valid_password, msg_password = check_password(password)
+        if not valid_password:
+            messages.append("Password:")
+            messages.append(self.transform_message(msg_password))
+
+        valid_username, msg_username = check_username(username)
+        if not valid_username:
+            messages.append("Username:")
+            messages.append(self.transform_message(msg_username))
+
+        valid_firstname, msg_firstname = check_firstname(firstname)
+        if not valid_firstname:
+            messages.append("Firstname:")
+            messages.append(self.transform_message(msg_firstname))
+
+        if gender is None:
+            messages.append("Gender:")
+            messages.append(self.transform_message("Please choose your gender"))
+
+        msg_box = QMessageBox()
+        msg_box.setText("\n".join(messages))
+        msg_box.exec_()
+
         # TODO: Create a new user with the given information
-        # username = self.line_edit_username.text()
-        # TODO: Check the password for the minimum requirements
-        # password = self.line_edit_password.text()
-        # firstname = self.line_edit_firstname.text()
-        # gender = self.gender
+        # create_new_user(
+        #     username=username,
+        #     password=password,
+        #     firstname=firstname,
+        #     gender=gender
+        # )
+
         self.show_login_window.emit(self)
+
+    @staticmethod
+    def transform_message(msg: str) -> str:
+        return "\n".join("        " + m for m in msg.split("\n"))
 
     def gender_button_state(self):
         gender_button = self.sender()
