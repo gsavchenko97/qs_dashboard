8c14fa47a4416fabc142275f70eaa2fffb598e48
BorisOne <boris95714@gmail.com>
2020-06-06 11:25:35 +0300

    added one more user_utils test

--- tests/test_user_utils.py
+++ tests/test_user_utils.py
@@ -18,6 +18,11 @@ def invalid_password():
     return "asdf()())()"
 
 
+@pytest.fixture
+def empty_password():
+    return ""
+
+
 def test_check_password_valid_password(valid_password):
     output = check_password(valid_password)
     assert isinstance(output, tuple), "check_password must return a tuple"
@@ -42,4 +47,13 @@ def test_check_password_invalid_password(invalid_password):
     assert len(output) == 2, "check_password must return flag and message as a tuple"
     valid, msg = output
     assert valid is False, f"'{invalid_password}' is not a valid password, but check_password says it is"
+    assert isinstance(msg, str), "check_password must return a string as a message"
+
+
+def test_check_password_empty_password(empty_password):
+    output = check_password(empty_password)
+    assert isinstance(output, tuple), "check_password must return a tuple"
+    assert len(output) == 2, "check_password must return flag and message as a tuple"
+    valid, msg = output
+    assert valid is False, f"'{empty_password}' is not a valid password, but check_password says it is"
     assert isinstance(msg, str), "check_password must return a string as a message"
