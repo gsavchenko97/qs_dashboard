25042b373ccb94b0117d8d6030a6428743b9953b
BorisOne <boris95714@gmail.com>
2020-06-07 17:21:44 +0300

    commented imports

--- qs_dashboard/__init__.py
+++ qs_dashboard/__init__.py
@@ -1,13 +1,13 @@
-from . import cli
-from . import controller
-from . import db
-from . import dialog
-from . import figure
-from . import gui
-from . import load_data_from_csv
-from . import login
-from . import measurement
-from . import signup
+# from . import cli
+# from . import controller
+# from . import db
+# from . import dialog
+# from . import figure
+# from . import gui
+# from . import load_data_from_csv
+# from . import login
+# from . import measurement
+# from . import signup
 
 
 __version__ = '1.0.0'
